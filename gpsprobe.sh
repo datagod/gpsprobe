ee53956e927b1e9748dd56cdac28f21606b1353d
datagod <william.mcevoy@gmail.com>
2019-07-14 20:53:58 -0400

    Add files via upload

---
+++ gpsprobe.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+clear
+
+
+RED="\e[31m"
+ORANGE="\e[33m"
+BLUE="\e[94m"
+GREEN="\e[92m"
+STOP="\e[0m"
+
+
+
+
+cd /home/pi/gpsprobe
+printf "${GREEN}"
+figlet "Launching Airmon"
+sudo airmon-ng start wlan1
+
+echo "checking for interfering processes"
+sudo airmon-ng check kill
+
+echo "renaming old logfile"
+mv gpslog.txt  "gpslog.$(date +%F_%R).txt "
+
+
+echo "Launch GPSProbe"
+sudo python gpsprobe.py -i wlan1mon -t iso  -o  gpslog.txt -f -s -r -l -g
+
+
+printf "${STOP}"
